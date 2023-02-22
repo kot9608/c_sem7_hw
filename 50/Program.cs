a3c812860b4b7dfaa870c377fd0a0cd0190345be
Kirill <Kot9608@mail.ru>
2023-02-22 16:21:37 +0500

    hw

---
+++ 50/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+// в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// [1, 7] -> такого числа в массиве нет
+
+//ПРИМЕЧАНИЕ!
+//Индекс элемента начинается с нуля!
+// Пример:
+// 1 2 3
+// 4 5 6
+// Индекс значения 1 = 0.
+
+
+using System;
+using static System.Console;
+
+Console.WriteLine("Введите количество строк массива: ");
+int rows=int.Parse(ReadLine());
+
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns=int.Parse(ReadLine());
+
+int[,] array = GetArrayUser(rows, columns, 1, 9);
+
+
+int[,] GetArrayUser(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.WriteLine($"Введите элемент массива: Строка {i} элемент {j}");
+            result[i, j] = int.Parse(ReadLine());
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
+Console.WriteLine(); 
+
+Console.WriteLine("Введенный массив");
+PrintArray(array);
+
+
+void FindElement(int[,] inArray)
+{
+  Console.WriteLine($"Введите строку для поиска элемента(начинается с 0): ");
+  int RowsFind=int.Parse(ReadLine());
+  Console.WriteLine($"Введите столбецдля поиска элемента(начинается с 0): ");
+  int RowsColumns=int.Parse(ReadLine());
+  if (RowsFind<inArray.GetLength(0) && RowsColumns<inArray.GetLength(1))
+  {
+    Console.WriteLine($"Элемент с индексом [{RowsFind}, {RowsColumns}] =  {(inArray[RowsFind, RowsColumns])}");
+  }
+  else
+  {
+    Console.WriteLine($"Элемента с индексом [{RowsFind}, {RowsColumns}] в введённом массиве нет");
+  }
+  //return inArray[RowsFind, RowsColumns];
+}
+
+FindElement(array);
---
+++ 52/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+//Задайте двумерный массив размером m×n, 
+//заполненный случайными целыми числами\
+
+using System;
+using static System.Console;
+
+Console.WriteLine("Введите количество строк массива: ");
+int rows=int.Parse(ReadLine());
+
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns=int.Parse(ReadLine());
+
+Console.WriteLine("Введите минимальное значение элемента массива: ");
+int min=int.Parse(ReadLine());
+
+Console.WriteLine("Введите максимальное значение элемента массива: ");
+int max=int.Parse(ReadLine());
+
+double[,] array = GetArray(rows, columns, min, max);
+
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double [,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble()*(maxValue-minValue)+minValue;//*(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{(inArray[i,j]):f2} ");
+        }
+        WriteLine();
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Сформирован массив: ");
+PrintArray(array);
+
+
+double [] SredArifmCol (double[,] inArray)
+{
+  double[] SredArifmColArray =new double[inArray.GetLength(1)];
+    for (int i = 0; i < inArray.GetLength(1); i++)
+    {
+      for (int j = 0; j < inArray.GetLength(0); j++)
+      {
+        SredArifmColArray[i]=SredArifmColArray[i]+inArray[j,i]/(inArray.GetLength(0));
+        SredArifmColArray[i]=Math.Round(SredArifmColArray[i], 2);
+       // SredArifmColArray[i]=((SredArifmColArray[i]):f2);
+      }
+       
+    }
+    
+    return SredArifmColArray;
+
+}
+double[] res1 = (SredArifmCol(array));
+//Console.WriteLine(SredArifmCol1(array));
+Console.WriteLine("Среднее арифмитическое каждого столбца: ");
+Console.WriteLine(String.Join(" ", res1));
+
